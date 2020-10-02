9acce14a9d4b3d2852b6ead6d0bd46427630f7c3
abhilash4384 <vickyabhilash4384@gmail.com>
2020-10-02 20:19:56 +0530

    UI modifications

---
+++ Models/PlayersModel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace YoYo_Web_App.Models
+{
+    public class PlayersModel
+    {
+        public int _id { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public int speedLevel { get; set; }
+
+        public int shuttleNumber { get; set; }
+
+    }
+}
---
+++ Services/DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoYo_Web_App.Models;
+
+namespace YoYo_Web_App.Services
+{
+    public class DataLoader
+    {
+        public IList<PlayersModel> LoadPlayersData()
+        {
+            PlayersModel[] listOfPlayers = new PlayersModel[15]
+            {
+               new PlayersModel() { firstName = "Ashton", lastName = "Eaton" },
+               new PlayersModel() { firstName = "Brayan", lastName = "Clay" },
+               new PlayersModel() { firstName = "Dean", lastName = "Karnazes" },
+               new PlayersModel() { firstName = "Usain", lastName = "Bolt" },
+               new PlayersModel() { firstName = "Dep", lastName = "Johny" },
+               new PlayersModel() { firstName = "Abhilash", lastName = "Virat" },
+               new PlayersModel() { firstName = "Sachin", lastName = "Tendulkar" },
+               new PlayersModel() { firstName = "James", lastName = "Bond" },
+               new PlayersModel() { firstName = "Steve", lastName = "Rogers" },
+               new PlayersModel() { firstName = "Scarlet", lastName = "Johnson" },
+               new PlayersModel() { firstName = "Emilie", lastName = "Clark" },
+               new PlayersModel() { firstName = "Diana", lastName = "Hayden" },
+               new PlayersModel() { firstName = "Salina", lastName = "Kayele" },
+               new PlayersModel() { firstName = "Ronaldo", lastName = "Christiano" },
+               new PlayersModel() { firstName = "Stephinie", lastName = "Mcmohan" }
+            };
+
+            return listOfPlayers.ToList<PlayersModel>();
+        }
+    }
+}
